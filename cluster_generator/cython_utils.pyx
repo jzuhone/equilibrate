3105ac861eec195c8d28245de09b1a0ccaa41abf
John ZuHone <jzuhone@gmail.com>
2016-02-08 10:37:57 -0500

    pseudo-progress bar for generating velocities

--- cluster_generator/cython_utils.pyx
+++ cluster_generator/cython_utils.pyx
@@ -19,7 +19,8 @@ cimport cython
 cdef extern from "math.h":
     double sqrt(double x) nogil
     double log10(double x) nogil
-
+    double fmod(double numer, double denom) nogil
+    
 cdef extern from "stdlib.h":
     double drand48() nogil
     void srand48(long int seedval) nogil
@@ -36,7 +37,7 @@ def generate_velocities(np.ndarray[DTYPE_t, ndim=1] psi,
                         f):
     cdef DTYPE_t v2, e
     cdef np.uint8_t not_done
-    cdef unsigned int i
+    cdef unsigned int i, p
     cdef int num_particles
     cdef long int seedval
     cdef np.ndarray[np.float64_t, ndim=1] velocity
@@ -52,4 +53,8 @@ def generate_velocities(np.ndarray[DTYPE_t, ndim=1] psi,
             e = psi[i]-0.5*v2
             not_done = f(e)*v2 < drand48()*fv2esc[i]
         velocity[i] = sqrt(v2)
+        p = int(fmod(float(i), float(num_particles/10)))
+        if p == 0:
+            p = int((100.*i)/float(num_particles)+0.5)
+            print("Generated %d percent of particle velocities." % p)
     return velocity
