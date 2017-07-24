87f3912ecd82036a7d4ee3054dd155786eda7b90
John ZuHone <jzuhone@gmail.com>
2017-07-24 10:31:45 -0400

    Refactor of virial model so that it only contains profiles and it writes particles as a separate operation

--- cluster_generator/virial.py
+++ cluster_generator/virial.py
@@ -1,5 +1,5 @@
 import numpy as np
-from yt import YTArray, YTQuantity, mylog, get_pbar
+from yt import YTArray, mylog, get_pbar
 from scipy.interpolate import InterpolatedUnivariateSpline
 from cluster_generator.utils import \
     G, quad, \
@@ -8,16 +8,17 @@ from cluster_generator.utils import \
 from cluster_generator.cluster_model import ClusterModel
 from cluster_generator.cython_utils import generate_velocities
 from collections import OrderedDict
+import os
 
 class VirialEquilibrium(ClusterModel):
 
     _type_name = "virial"
 
     @classmethod
-    def from_scratch(cls, num_particles, rmin, rmax, profile,
-                     num_points=1000):
+    def from_scratch(cls, rmin, rmax, profile, num_points=1000):
 
-        rr = np.logspace(np.log10(rmin), np.log10(rmax), num_points, endpoint=True)
+        rr = np.logspace(np.log10(rmin), np.log10(rmax), 
+                         num_points, endpoint=True)
 
         pden = profile(rr)
         mylog.info("Integrating dark matter mass profile.")
@@ -26,22 +27,21 @@ class VirialEquilibrium(ClusterModel):
         gpot_profile = lambda r: profile(r)*r
         gpot = G.v*(mdm/rr + 4.*np.pi*integrate_toinf(gpot_profile, rr))
 
-        return cls(num_particles, rr, gpot, pden, mdm)
+        return cls(rr, gpot, pden, mdm)
 
     @classmethod
-    def from_hse_model(cls, num_particles, hse_model):
+    def from_hse_model(cls, hse_model):
         gpot = -hse_model["gravitational_potential"].in_units("kpc**2/Myr**2")
-        return cls(num_particles, hse_model["radius"].v,
-                   gpot.v, hse_model["dark_matter_density"].v,
+        return cls(hse_model["radius"].v, gpot.v, 
+                   hse_model["dark_matter_density"].v,
                    hse_model["dark_matter_mass"].v)
 
-    def __init__(self, num_particles, rr, gpot, pden, mdm):
+    def __init__(self, rr, gpot, pden, mdm):
 
         fields = OrderedDict()
 
         ee = gpot[::-1]
         density_spline = InterpolatedUnivariateSpline(ee, pden[::-1])
-        energy_spline = InterpolatedUnivariateSpline(rr, gpot)
 
         num_points = gpot.shape[0]
 
@@ -59,43 +59,15 @@ class VirialEquilibrium(ClusterModel):
         self.f = f
         self.rr = rr
         self.pden = pden
+        self.mdm = mdm
 
-        mylog.info("We will be assigning %d particles." % num_particles)
-        mylog.info("Compute particle positions.")
+        fields["radius"] = YTArray(self.rr, "kpc")
+        fields["dark_matter_density"] = YTArray(pden, "Msun/kpc**3")
+        fields["dark_matter_mass"] = YTArray(mdm, "Msun")
+        fields["gravitational_potential"] = YTArray(-ee, "kpc**2/Myr**2")
+        fields["distribution_function"] = YTArray(f(ee), "Msun*Myr**3/kpc**6")
 
-        u = np.random.uniform(size=num_particles)
-        P_r = np.insert(mdm, 0, 0.0)
-        P_r /= P_r[-1]
-        radius = np.interp(u, P_r, np.insert(rr, 0, 0.0), left=0.0, right=1.0)
-
-        theta = np.arccos(np.random.uniform(low=-1.,high=1.,size=num_particles))
-        phi = 2.*np.pi*np.random.uniform(size=num_particles)
-
-        fields["particle_radius"] = YTArray(radius, "kpc")
-        fields["particle_position_x"] = YTArray(radius*np.sin(theta)*np.cos(phi), "kpc")
-        fields["particle_position_y"] = YTArray(radius*np.sin(theta)*np.sin(phi), "kpc")
-        fields["particle_position_z"] = YTArray(radius*np.cos(theta), "kpc")
-
-        mylog.info("Compute particle velocities.")
-
-        psi = energy_spline(radius)
-        vesc = 2.*psi
-        fv2esc = vesc*f(psi)
-        vesc = np.sqrt(vesc)
-        velocity = generate_velocities(psi, vesc, fv2esc, f)
-        theta = np.arccos(np.random.uniform(low=-1.,high=1.,size=num_particles))
-        phi = 2.*np.pi*np.random.uniform(size=num_particles)
-
-        fields["particle_velocity"] = YTArray(velocity, "kpc/Myr")
-        fields["particle_velocity_x"] = YTArray(velocity*np.sin(theta)*np.cos(phi), "kpc/Myr")
-        fields["particle_velocity_y"] = YTArray(velocity*np.sin(theta)*np.sin(phi), "kpc/Myr")
-        fields["particle_velocity_z"] = YTArray(velocity*np.cos(theta), "kpc/Myr")
-
-        fields["particle_mass"] = YTArray([mdm.max()/num_particles], "Msun")
-        fields["particle_potential"] = YTArray(psi, "kpc**2/Myr**2")
-        fields["particle_energy"] = fields["particle_potential"]-0.5*fields["particle_velocity"]**2
-
-        super(VirialEquilibrium, self).__init__(num_particles, fields)
+        super(VirialEquilibrium, self).__init__(num_points, fields)
 
     def check_model(self):
         n = len(self.ee)
@@ -107,3 +79,61 @@ class VirialEquilibrium(ClusterModel):
         mylog.info("The maximum relative deviation of this profile from "
                    "virial equilibrium is %g" % np.abs(chk).max())
         return rho, chk
+
+    def generate_particles(self, filename, num_particles, overwrite=False):
+        """
+        Generate a set of dark matter particles in virial equilibrium
+        and write them to an HDF5 file.
+
+        Parameters
+        ----------
+        filename : string
+            The filename to write the particle file to. 
+        num_particles : integer
+            The number of particles to generate.
+        overwrite : boolean, optional
+            If True, overwrite a file with the same name.
+        """
+        if not overwrite and os.path.exists(filename):
+            raise IOError("Filename %s already exists and overwrite=False!" % filename)
+        energy_spline = InterpolatedUnivariateSpline(self.rr, self.ee[::-1])
+
+        mylog.info("We will be assigning %d particles." % num_particles)
+        mylog.info("Compute particle positions.")
+
+        u = np.random.uniform(size=num_particles)
+        P_r = np.insert(self.mdm, 0, 0.0)
+        P_r /= P_r[-1]
+        radius = np.interp(u, P_r, np.insert(self.rr, 0, 0.0), left=0.0, right=1.0)
+
+        theta = np.arccos(np.random.uniform(low=-1., high=1., size=num_particles))
+        phi = 2.*np.pi*np.random.uniform(size=num_particles)
+
+        fields = OrderedDict()
+
+        fields["particle_radius"] = YTArray(radius, "kpc")
+        fields["particle_position_x"] = YTArray(radius*np.sin(theta)*np.cos(phi), "kpc")
+        fields["particle_position_y"] = YTArray(radius*np.sin(theta)*np.sin(phi), "kpc")
+        fields["particle_position_z"] = YTArray(radius*np.cos(theta), "kpc")
+
+        mylog.info("Compute particle velocities.")
+
+        psi = energy_spline(radius)
+        vesc = 2.*psi
+        fv2esc = vesc*self.f(psi)
+        vesc = np.sqrt(vesc)
+        velocity = generate_velocities(psi, vesc, fv2esc, self.f)
+        theta = np.arccos(np.random.uniform(low=-1., high=1., size=num_particles))
+        phi = 2.*np.pi*np.random.uniform(size=num_particles)
+
+        fields["particle_velocity"] = YTArray(velocity, "kpc/Myr")
+        fields["particle_velocity_x"] = YTArray(velocity*np.sin(theta)*np.cos(phi), "kpc/Myr")
+        fields["particle_velocity_y"] = YTArray(velocity*np.sin(theta)*np.sin(phi), "kpc/Myr")
+        fields["particle_velocity_z"] = YTArray(velocity*np.cos(theta), "kpc/Myr")
+
+        fields["particle_mass"] = YTArray([self.mdm.max()/num_particles], "Msun")
+        fields["particle_potential"] = -YTArray(psi, "kpc**2/Myr**2")
+        fields["particle_energy"] = fields["particle_potential"]+0.5*fields["particle_velocity"]**2
+
+        for name, array in fields.items():
+            array.write_hdf5(filename, dataset_name=name)
