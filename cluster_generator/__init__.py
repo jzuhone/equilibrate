935c41197f797a45b2df56bd774aa151ab818170
John ZuHone <jzuhone@gmail.com>
2017-09-22 14:54:53 -0400

    Many improvements to gaussian random fields

--- cluster_generator/__init__.py
+++ cluster_generator/__init__.py
@@ -1,3 +1,12 @@
-from cluster_generator.cluster_model import ClusterModel, ClusterParticles
-from cluster_generator.hydrostatic import HydrostaticEquilibrium
-from cluster_generator.virial import VirialEquilibrium
+from cluster_generator.cluster_model import \
+    ClusterModel, \
+    ClusterParticles
+from cluster_generator.hydrostatic import \
+    HydrostaticEquilibrium
+from cluster_generator.virial import \
+    VirialEquilibrium
+from cluster_generator.gaussian_random_field import \
+    GaussianRandomField, \
+    RandomMagneticField, \
+    RandomMagneticVectorPotential, \
+    RandomVelocityField
--- cluster_generator/gaussian_random_field.py
+++ cluster_generator/gaussian_random_field.py
@@ -1,14 +1,15 @@
 import numpy as np
 from yt.units.yt_array import YTArray
-from yt.funcs import iterable
+import os
 
 def parse_value(value, default_units):
     if isinstance(value, YTArray):
-        return YTArray(value.v, value.units).in_units(default_units)
+        val = YTArray(value.v, value.units).in_units(default_units)
     elif isinstance(value, tuple):
-        return YTArray(value[0], value[1]).in_units(default_units)
+        val = YTArray(value[0], value[1]).in_units(default_units)
     else:
-        return YTArray(value, default_units)
+        val = YTArray(value, default_units)
+    return val.v
 
 def rot_3d(axis, x1, y1, z1, ang):
 
@@ -31,29 +32,35 @@ def rot_3d(axis, x1, y1, z1, ang):
     return x2, y2, z2
 
 class GaussianRandomField(object):
-    def __init__(self, left_edge, right_edge, ddims, l_min, l_max,
-                 avg_A=1.0, ctr1=None, ctr2=None, r1=None,
-                 r2=None, A1=None, A2=None, prng=np.random):
+    _units = "dimensionless"
+    _name = "vector"
 
-        le = parse_value(left_edge, "kpc").v
-        re = parse_value(right_edge, "kpc").v
+    def __init__(self, left_edge, right_edge, ddims, l_min, l_max,
+                 g_rms=1.0, ctr1=None, ctr2=None, r1=None,
+                 r2=None, g1=None, g2=None, vector_potential=False,
+                 prng=np.random):
+
+        self.vector_potential = vector_potential
+
+        le = parse_value(left_edge, "kpc")
+        re = parse_value(right_edge, "kpc")
 
         nx, ny, nz = ddims
 
         num_halos = 0
         if ctr1 is not None:
-            ctr1 = parse_value(ctr1, "kpc").v
+            ctr1 = parse_value(ctr1, "kpc")
             num_halos = 1
         if ctr2 is not None:
             num_halos = 2
-            ctr2 = parse_value(ctr2, "kpc").v
+            ctr2 = parse_value(ctr2, "kpc")
 
         # Derived stuff
 
         dx, dy, dz = (re-le)/ddims
 
-        l_min = parse_value(l_min, "kpc").v
-        l_max = parse_value(l_max, "kpc").v
+        l_min = parse_value(l_min, "kpc")
+        l_max = parse_value(l_max, "kpc")
 
         k0 = 2.*np.pi/l_min
         k1 = 2.*np.pi/l_max
@@ -97,19 +104,19 @@ class GaussianRandomField(object):
         v[:,0,ny-1:ny//2:-1,0] = np.conjugate(v[:,0,1:ny//2,0])
         v[:,0,0,nz-1:nz//2:-1] = np.conjugate(v[:,0,0,1:nz//2])
 
-        ax = np.fft.ifftn(sigma*v[0,:,:,:]).real
-        ay = np.fft.ifftn(sigma*v[1,:,:,:]).real
-        az = np.fft.ifftn(sigma*v[2,:,:,:]).real
+        gx = np.fft.ifftn(sigma*v[0,:,:,:]).real
+        gy = np.fft.ifftn(sigma*v[1,:,:,:]).real
+        gz = np.fft.ifftn(sigma*v[2,:,:,:]).real
 
         del sigma, v
 
-        Aavg = np.std(np.sqrt(ax*ax+ay*ay+az*az))
+        g_avg = np.std(np.sqrt(gx*gx+gy*gy+gz*gz))
 
-        ax /= Aavg
-        ay /= Aavg
-        az /= Aavg
+        gx /= g_avg
+        gy /= g_avg
+        gz /= g_avg
 
-        x,y,z = np.mgrid[0:nx,0:ny,0:nz] + 0.5
+        x, y, z = np.mgrid[0:nx,0:ny,0:nz] + 0.5
         x *= dx
         y *= dy
         z *= dz
@@ -118,69 +125,157 @@ class GaussianRandomField(object):
         z += le[2]
 
         if num_halos == 0:
-            avg_A = parse_value(avg_A, "gauss").v
+            g_rms = parse_value(g_rms, self._units)
         else:
             if num_halos == 1:
                 rr1 = np.sqrt((x-ctr1[0])**2 + (y-ctr1[1])**2 + (z-ctr1[2])**2)
                 idxs1 = np.searchsorted(r1, rr1) - 1
                 dr1 = (rr1-r1[idxs1])/(r1[idxs1+1]-r1[idxs1])
-                avg_A = ((1.-dr1)*A1[idxs1] + dr1*A1[idxs1+1])**2
+                g_rms = ((1.-dr1)*g1[idxs1] + dr1*g1[idxs1+1])**2
             if num_halos == 2:
                 rr2 = np.sqrt((x-ctr2[0])**2 + (y-ctr2[1])**2 + (z-ctr2[2])**2)
                 idxs2 = np.searchsorted(r2, rr2) - 1
                 dr2 = (rr2-r2[idxs2])/(r2[idxs2+1]-r2[idxs2])
-                avg_A += ((1.-dr2)*A2[idxs2] + dr2*A2[idxs2+1])**2
-                avg_A = np.sqrt(avg_A)
+                g_rms += ((1.-dr2)*g2[idxs2] + dr2*g2[idxs2+1])**2
+            g_rms = np.sqrt(g_rms).to_units(self._units).d
 
-        ax *= avg_A
-        ay *= avg_A
-        az *= avg_A
-
-        field_units = str(avg_A.units)
+        gx *= g_rms
+        gy *= g_rms
+        gz *= g_rms
 
         self.data = {}
         self.data["x"] = YTArray(x, "kpc")
         self.data["y"] = YTArray(y, "kpc")
         self.data["z"] = YTArray(z, "kpc")
 
-        del x,y,z,avg_A
+        del x, y, z, g_rms
 
-        # Rotate vector potential
+        if self.vector_potential:
 
-        axk = np.fft.fftn(ax)
-        ayk = np.fft.fftn(ay)
-        azk = np.fft.fftn(az)
+            # Rotate vector potential
 
-        with np.errstate(invalid='ignore', divide='ignore'):
-            alpha = np.arccos(kx/np.sqrt(kx*kx+ky*ky))
-        alpha[ky < 0.0] -= 2.0*np.pi
-        alpha[ky < 0.0] *= -1.
-        with np.errstate(invalid='ignore', divide='ignore'):
-            beta = np.arccos(kz/kk)
-        alpha[np.isinf(alpha)] = 0.0
-        alpha[np.isnan(alpha)] = 0.0
-        beta[np.isnan(beta)] = 0.0
-        beta[np.isinf(beta)] = 0.0
+            gxk = np.fft.fftn(gx)
+            gyk = np.fft.fftn(gy)
+            gzk = np.fft.fftn(gz)
 
-        axk, ayk, azk = rot_3d(3, axk, ayk, azk, alpha)
-        axk, ayk, azk = rot_3d(2, axk, ayk, azk, beta)
+            with np.errstate(invalid='ignore', divide='ignore'):
+                alpha = np.arccos(kx/np.sqrt(kx*kx+ky*ky))
+            alpha[ky < 0.0] -= 2.0*np.pi
+            alpha[ky < 0.0] *= -1.
+            with np.errstate(invalid='ignore', divide='ignore'):
+                beta = np.arccos(kz/kk)
+            alpha[np.isinf(alpha)] = 0.0
+            alpha[np.isnan(alpha)] = 0.0
+            beta[np.isnan(beta)] = 0.0
+            beta[np.isinf(beta)] = 0.0
 
-        with np.errstate(invalid='ignore', divide='ignore'):
-            axk = (0.0+1.0j)*ayk/kk
-            ayk = -(0.0+1.0j)*axk/kk
-            azk = np.zeros(axk.shape, dtype="complex")
+            gxk, gyk, gzk = rot_3d(3, gxk, gyk, gzk, alpha)
+            gxk, gyk, gzk = rot_3d(2, gxk, gyk, gzk, beta)
 
-        axk[np.isinf(axk)] = 0.0
-        axk[np.isnan(axk)] = 0.0
-        ayk[np.isinf(ayk)] = 0.0
-        ayk[np.isnan(ayk)] = 0.0
+            with np.errstate(invalid='ignore', divide='ignore'):
+                gxk = (0.0+1.0j)*gyk/kk
+                gyk = -(0.0+1.0j)*gxk/kk
+                gzk = np.zeros(gxk.shape, dtype="complex")
 
-        axk, ayk, azk = rot_3d(2, axk, ayk, azk,  -beta)
-        axk, ayk, azk = rot_3d(3, axk, ayk, azk, -alpha)
+            gxk[np.isinf(gxk)] = 0.0
+            gxk[np.isnan(gxk)] = 0.0
+            gyk[np.isinf(gyk)] = 0.0
+            gyk[np.isnan(gyk)] = 0.0
 
-        self.data["vector_x"] = YTArray(np.real(np.fft.ifftn(axk)), "%s*kpc" % field_units)
-        self.data["vector_y"] = YTArray(np.real(np.fft.ifftn(ayk)), "%s*kpc" % field_units)
-        self.data["vector_z"] = YTArray(np.real(np.fft.ifftn(azk)), "%s*kpc" % field_units)
+            gxk, gyk, gzk = rot_3d(2, gxk, gyk, gzk,  -beta)
+            gxk, gyk, gzk = rot_3d(3, gxk, gyk, gzk, -alpha)
+
+            self.data[self._name+"_x"] = YTArray(np.fft.ifftn(gxk).real, "%s*kpc" % self._units)
+            self.data[self._name+"_y"] = YTArray(np.fft.ifftn(gyk).real, "%s*kpc" % self._units)
+            self.data[self._name+"_z"] = YTArray(np.fft.ifftn(gzk).real, "%s*kpc" % self._units)
+
+        else:
+
+            self.data[self._name+"_x"] = YTArray(gx, self._units)
+            self.data[self._name+"_y"] = YTArray(gy, self._units)
+            self.data[self._name+"_z"] = YTArray(gz, self._units)
 
     def __getitem__(self, item):
         return self.data[item]
+
+    @property
+    def units(self):
+        if self.vector_potential:
+            return "%s*kpc" % self._units
+        else:
+            return self._units
+
+    def write_to_h5(self, filename, in_cgs=False, overwrite=False):
+        import h5py
+        if os.path.exists(filename) and not overwrite:
+            raise IOError("Cannot create %s. It exists and overwrite=False." % filename)
+        for field in self.data:
+            if in_cgs:
+                self.data[field].in_cgs().write_hdf5(filename, dataset_name=field)
+            else:
+                self.data[field].write_hdf5(filename, dataset_name=field)
+        f = h5py.File(filename, "r+")
+        f.attrs["name"] = self._name
+        f.attrs["units"] = self.units
+        f.attrs["vector_potential"] = int(self.vector_potential)
+        f.flush()
+        f.close()
+
+class RandomMagneticField(GaussianRandomField):
+    _units = "gauss"
+    _name = "magnetic_field"
+    _vector_potential = False
+
+    def __init__(self, left_edge, right_edge, ddims, l_min, l_max,
+                 B_rms=1.0, ctr1=None, ctr2=None, profile1=None,
+                 profile2=None, prng=np.random):
+        if profile1 is None:
+            r1 = None
+            B1 = None
+        else:
+            r1 = YTArray.from_hdf5(profile1, dataset_name="radius",
+                                   group_name="fields")
+            B1 = YTArray.from_hdf5(profile1, dataset_name="magnetic_field_strength",
+                                   group_name="fields")
+        if profile2 is None:
+            r2 = None
+            B2 = None
+        else:
+            r2 = YTArray.from_hdf5(profile2, dataset_name="radius",
+                                   group_name="fields")
+            B2 = YTArray.from_hdf5(profile2, dataset_name="magnetic_field_strength",
+                                   group_name="fields")
+        super(RandomMagneticField, self).__init__(left_edge, right_edge, 
+            ddims, l_min, l_max, g_rms=B_rms, ctr1=ctr1, ctr2=ctr2, r1=r1, 
+            r2=r2, g1=B1, g2=B2, vector_potential=self._vector_potential, prng=prng)
+
+class RandomMagneticVectorPotential(GaussianRandomField):
+    _name = "magnetic_vector_potential"
+    _vector_potential = True
+
+class RandomVelocityField(GaussianRandomField):
+    _units = "kpc/Myr"
+    _name = "velocity"
+
+    def __init__(self, left_edge, right_edge, ddims, l_min, l_max,
+                 V_rms=1.0, ctr1=None, ctr2=None, profile1=None,
+                 profile2=None, prng=np.random):
+        if profile1 is None:
+            r1 = None
+            V1 = None
+        else:
+            r1 = YTArray.from_hdf5(profile1, dataset_name="radius",
+                                   group_name="fields")
+            V1 = YTArray.from_hdf5(profile1, dataset_name="velocity_dispersion",
+                                   group_name="fields")
+        if profile2 is None:
+            r2 = None
+            V2 = None
+        else:
+            r2 = YTArray.from_hdf5(profile2, dataset_name="radius",
+                                   group_name="fields")
+            V2 = YTArray.from_hdf5(profile2, dataset_name="velocity_dispersion",
+                                   group_name="fields")
+        super(RandomVelocityField, self).__init__(left_edge, right_edge, ddims, 
+            l_min, l_max, g_rms=V_rms, ctr1=ctr1, ctr2=ctr2, r1=r1, r2=r2, g1=V1, 
+            g2=V2, prng=prng)
