ccb85a1b50b4c13824f1b9777450fa37256a7e88
John ZuHone <jzuhone@gmail.com>
2020-08-18 11:51:57 -0400

    Add this dependence

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(name='cluster_generator',
       author_email='jzuhone@gmail.com',
       url='http://github.com/jzuhone/cluster_generator',
       download_url='https://github.com/jzuhone/cluster_generator/tarball/0.1.0',
-      install_requires=["six","numpy","scipy","yt","cython"],
+      install_requires=["numpy","scipy","yt","cython","ruamel.yaml"],
       classifiers=[
           'Intended Audience :: Science/Research',
           'Operating System :: OS Independent',
