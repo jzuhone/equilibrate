3af3609d106969f6bc78a2113edd420f712419d8
John ZuHone <jzuhone@gmail.com>
2021-08-05 11:21:17 -0400

    These should not be public

--- cluster_generator/__init__.py
+++ cluster_generator/__init__.py
@@ -3,9 +3,7 @@ from cluster_generator.cluster_model import \
 from cluster_generator.cluster_ics import \
     ClusterICs
 from cluster_generator.cluster_particles import \
-    ClusterParticles, combine_two_clusters, \
-    combine_three_clusters, resample_one_cluster, \
-    resample_two_clusters, resample_three_clusters
+    ClusterParticles
 from cluster_generator.hydrostatic import \
     HydrostaticEquilibrium
 from cluster_generator.virial import \
