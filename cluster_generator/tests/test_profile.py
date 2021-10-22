a50bbed466a713928cf60f8c6dc95eb358491817
John ZuHone <jzuhone@gmail.com>
2021-10-22 11:55:07 -0400

    Check the HSE accuracy

--- cluster_generator/tests/test_profile.py
+++ cluster_generator/tests/test_profile.py
@@ -5,5 +5,4 @@ from cluster_generator.tests.utils import model_answer_testing, \
 def test_model(answer_store, answer_dir):
     m = generate_model()
     model_answer_testing(m, "profile.h5", answer_store, answer_dir)
-    #profile_answer_testing(vd, "virial_dm.h5", answer_store, answer_dir)
-    #profile_answer_testing(vs, "virial_star.h5", answer_store, answer_dir)
+    assert m.check_hse() < 1.0e-4
