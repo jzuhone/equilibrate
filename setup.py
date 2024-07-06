712e41552860713a6130a23a000dea8d89a0b6d8
Nathan Diggins <eliza.diggins@utah.edu>
2024-07-06 14:50:33 -0600

    trying to fix change in dtype size (likely due to numpy 2.0)

--- setup.py
+++ setup.py
@@ -29,7 +29,15 @@ setup(
     author_email="jzuhone@gmail.com",
     url="https://github.com/jzuhone/cluster_generator",
     download_url="https://github.com/jzuhone/cluster_generator/tarball/0.1.0",
-    install_requires=["numpy", "scipy", "yt", "unyt", "cython", "ruamel.yaml", "h5py"],
+    install_requires=[
+        "numpy<2",
+        "scipy",
+        "yt",
+        "unyt",
+        "cython",
+        "ruamel.yaml",
+        "h5py",
+    ],
     classifiers=[
         "Intended Audience :: Science/Research",
         "Operating System :: OS Independent",
